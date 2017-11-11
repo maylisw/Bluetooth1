f9feb7ea76b407698a9077e932de3534aef391a3
michaelxiong <michaelxiong2001Gmail.com>
2017-11-11 13:59:22 -0800

    Michael 1: Enabling Bluetooth

--- app/src/main/java/com/example/michaelxiong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/michaelxiong/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.michaelxiong.myapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpBT();
+    }
+
+    protected void setUpBT(){
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "This device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(this, "Please enable Bluetooth NOW!!!!!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Yay", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("Working:", "Bluetooth enabled");
+        Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
     }
 }
